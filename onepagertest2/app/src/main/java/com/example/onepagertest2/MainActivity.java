81bf65b401dfb70e66ebfa400cee2a253fad444d
Lvanhuijstee <2132008@talnet.nl>
2022-01-20 14:10:15 +0100

    ok

--- onepagertest2/app/src/main/java/com/example/onepagertest2/MainActivity.java
+++ onepagertest2/app/src/main/java/com/example/onepagertest2/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
     // this goes to aboutmeScene
     private Scene aboutMeScene = null;
     // this goes to Homescreen
@@ -26,14 +25,10 @@ public class MainActivity extends AppCompatActivity {
     // scene 4
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button;
-        button = findViewById(R.id.myworkButton);
 
         // scene 1
         homeScene = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.home_activity_scene, this);
