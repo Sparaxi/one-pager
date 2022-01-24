9ccd7fe892d5707a86610989aa2551f73ed77065
wesley <wvdrp@hotmail.com>
2022-01-24 21:13:19 +0100

    Lots of changes + redesign

--- onepagertest2/app/src/main/java/com/example/onepagertest2/MainActivity.java
+++ onepagertest2/app/src/main/java/com/example/onepagertest2/MainActivity.java
@@ -16,130 +16,64 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    // scene 1
-    private Scene aboutMeScene = null;
-    // scene 2
-    private Scene homeScene = null;
-    // scene 3
-    private Scene myWorkScene = null;
-    // scene 4
-    private Scene skillsScene = null;
-    // scene 5
-    private Scene contactScene = null;
-    //scene  6
-    private Scene example = null;
 
-//    private ActionBar actionBar;
-//    private ViewPager viewPager;
-//    private ArrayList<SkillsCardViewStringStorage> skillsArrayList;
-//    private SkillsCardViewAdapter skillsCardViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // scene 1
-        homeScene = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.home_activity_scene, this);
-        // scene 2
-        aboutMeScene = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.overmij_scene, this);
-        // scene 3
-        myWorkScene = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.mywork, this);
-        // scene 4
-        skillsScene = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.skills_scene, this);
-        // scene 5
-        contactScene = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.gegevens_scene, this);
-        // scene6
-        example = Scene.getSceneForLayout((ViewGroup) findViewById(R.id.rootContainer), R.layout.myworkexample, this);
+        TextView aboutMeText;
+        TextView skillsText;
+        TextView expText;
+        TextView ContactText;
 
-        homeScene.enter();
+        aboutMeText = findViewById(R.id.aboutmeText);
+        skillsText = findViewById(R.id.skillsText);
+        expText = findViewById(R.id.expText);
+        ContactText = findViewById(R.id.ContactText);
 
-//        actionBar = getSupportActionBar();
-//
-//        viewPager = findViewById(R.id.viewPager);
-//        loadCards();
-//
-//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
 
-    }
+        ImageView aboutMeButton = null;
+        ImageView skillsButton = null;
+        ImageView experienceButton = null;
+        ImageView contactButton = null;
 
-//    private void loadCards(){
-//        skillsArrayList = new ArrayList<>();
-//
-//        skillsArrayList.add(new SkillsCardViewStringStorage("test","test",R.drawable.custom_button));
-//
-//        skillsCardViewAdapter = new SkillsCardViewAdapter(this, skillsArrayList);
-//
-//        viewPager.setAdapter(skillsCardViewAdapter);
-//        viewPager.setPadding(100, 0, 100, 0);
-//    }
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void myWorkButton(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(myWorkScene, explode);
-    }
-    public void contactButton(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(contactScene, explode);
-    }
-    public void aboutMeButton(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(aboutMeScene, explode);
-    }
+            }
+        });
 
-    public void skillsButton(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(skillsScene, explode);
-    }
+        skillsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void home_From_Skills(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(homeScene, explode);
-    }
+            }
+        });
 
-    public void home_from_myWork(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(homeScene, explode);
-    }
+        experienceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void home_from_contact(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(homeScene, explode);
-    }
+            }
+        });
 
-    public void home_from_aboutMe(View view) {
-        Transition explode = new Explode();
-        TransitionManager.go(homeScene, explode);
-    }
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void goExample(View view) {
-        Transition fade = new Fade();
-        TransitionManager.go(example, fade);
-    }
+            }
+        });
 
-    public void backToWork(View view) {
-        Transition fade = new Fade();
-        TransitionManager.go(myWorkScene, fade);
     }
 
 }
